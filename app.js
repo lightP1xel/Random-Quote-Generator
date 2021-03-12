1e15363ab13080b8fcc4ad1f073219676e25e4e9
Ilya <ilya.naumenko53@gmail.com>
2021-03-12 21:05:59 +0300

    fix url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const authorQuote = document.querySelector('.authorContainer')
 const generateBtn = document.querySelector('.createQuote')
 
 async function getRandomQuote() {
-  let response = await fetch('http://quotable.io/random')
+  let response = await fetch('https://quotable.io/random')
   let result = await response.json()
   randomQuote.innerHTML = result.content
   authorQuote.innerHTML = `Author: ${result.author}`
