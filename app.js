89bc15c51f7abcd16971aca6aade9057b05bb2b5
Ilya <ilya.naumenko53@gmail.com>
2021-03-12 19:46:20 +0300

    Update repo

---
+++ app.js
@@ -0,0 +1,12 @@
+const randomQuote = document.querySelector('.randomQuoteContainer')
+const authorQuote = document.querySelector('.authorContainer')
+const generateBtn = document.querySelector('.createQuote')
+
+async function getRandomQuote() {
+  let response = await fetch('http://quotable.io/random')
+  let result = await response.json()
+  randomQuote.innerHTML = result.content
+  authorQuote.innerHTML = `Author: ${result.author}`
+}
+
+generateBtn.addEventListener('click', getRandomQuote)
